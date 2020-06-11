9d40d2ca963fa4937d66f827a846beab4fab1a38
Pascal Tassel <tasselp@gmail.com>
2020-06-11 19:09:12 +0200

    updated autolad array

--- app/config/config.php
+++ app/config/config.php
@@ -5,6 +5,11 @@ if(!defined('IZY')) die('DIRECT ACCESS FORBIDDEN');
 // Core
 $config['host']	                  = '';   // www.example.com
 $config['hooks']                  = [];   // Enabled hooks
-$config['autoload']['helpers']    = ['Url_helper'];
+$config['autoload'] = [
+  'databases' => [],
+  'helpers' => ['Url_helper'],
+  'models' => [],
+  'libraries' => []
+];
 $config['libraries']              = [];   // Libraries settings
 $config['db']                     = [];   // Database settings
