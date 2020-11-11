8ba52a7f7f60ba986af3e91297e0330b8cb1d73a
Pascal Tassel <tasselp@gmail.com>
2020-11-11 21:51:27 +0100

    remove all  vars except ['host']

--- app/config/config.php
+++ app/config/config.php
@@ -4,14 +4,3 @@ if(!defined('IZY')) die('DIRECT ACCESS FORBIDDEN');
 
 // Core
 $config['host'] = '';       // www.example.com
-$config['hooks'] = [];      // Enabled hooks
-$config['autoload'] = [
-    'databases' => [],
-    'helpers' => ['Url_helper'],
-    'models' => [],
-    'libraries' => [
-        //'Breadcrumb' => [],
-        //'Pagination' => ['alias' => 'page', 'limit' => 20, 'range' => 2, 'source' => 'query']
-    ]
-];
-$config['databases'] = [];  // Database settings
