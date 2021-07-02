bc5cd48f81e75182e9e5ae08a05ff36df0839b1a
Pascal Tassel <tasselp@gmail.com>
2021-07-02 22:57:15 +0200

    update comments

--- core/Izy.php
+++ core/Izy.php
@@ -1,24 +1,57 @@
 <?php
-
 namespace core;
 
 if(!defined('IZY')) die('DIRECT ACCESS FORBIDDEN');
 
-// Global functions
+/**
+* This file is part of the Izy_mvc project.
+*
+* Set an instance of main controller IZY_Controller
+* 1 - All system classes, models and helpers are loaded from the constructor and defined as attributes
+* 2 - IZY_Url class get request
+* 3 - IZY_Router class build response according to the request (define class, method and arguments)
+* 4 - Response controller (which extends main controller) is called and performs the output
+* 5 - IZY_Http set HTTP header code according to the response (200 or 404)
+* 5 - IZY_Http add optionnals HTTP headers (defined in response controller)
+* 6 - IZY_Output write response or show 404 (if defined)
+*
+* @package Izy-mvc
+* @copyright 2021 © Pascal Tassel for https://www.izy-mvc.com <contact[@]izy-mvc.com>
+*/
+
+// Load Global functions
 require_once(DIR_PATH . 'core' . DIRECTORY_SEPARATOR . 'Functions.php');
 
-// Autoloading
+// Get Autoloading files (defined in $config['autoloading'])
 $autoloading = get_config('autoloading');
-if ( !is_null($autoloading) ) {
-    
-    foreach ($autoloading as $autoload) {
+if (!is_null($autoloading)) 
+{
+    foreach ($autoloading as $autoload)
+    {
+        // File exist ?
+        if (file_exists(DIR_PATH . $autoload) === false)
+        {
+            throw new system\IZY_Exception('$config[\'autoload\'] doit être un tableau associatif. Exemple&nbsp;: $config[\'autoload\'][\'helpers\']');
+        }
+        // Include autoload file
         require_once(DIR_PATH . $autoload);
     }
 }
 
 /**
-* IZY Object
-* @author Pascal Tassel : https://www.izy-mvc.com
+* IZY class performs different tasks:
+*
+* 1 - Store an instance of each Izy system class in an attribute using load_class() global function.
+* 2 - Load helpers and models defined in $config['autoload'] using load_class() global function.
+*    (A trace of each loaded class with load_class() is stored in an array in system_loaded() global function)
+* 3 - Send HTTP Headers (depending on the response of the router)
+* 4 - Call response controller (depending on the response of the router) or 404 controller (if defined in $routes['404_url'])
+* 5 - Add canonical meta tag in output (depending on the response of the router)
+* 6 - Write response (depending on the response controller)
+*
+*   (Various hooks classes, defined in $config['hooks'] are called between this steps)
+*
+* @author Pascal Tassel <contact[@]izy-mvc.com>
 */
 class IZY
 {
@@ -26,102 +59,128 @@ class IZY
 
     private function __construct()
     {
-        // Hooks
+        // Define Izy_Hooks as attribute
         $this->hooks =& load_class('Hooks');
 
-        // Pre-system hook
+        // Call Pre_system class
         $this->hooks->set_hook('pre_system');
 
-        // Requests system classes as IZY attributes
+        // Define Izy_Url as attribute
         $this->url =& load_class('Url');
+        // Define Izy_Router  as attribute (passing the request as argument)
         $this->router =& load_class('Router', 'system', $this->url->request);
 
-        // Responses system classes as attributes
+        // Define remaining Izy classes as attributes
         $this->http =& load_class('Http');
         $this->output =& load_class('Output');
         $this->load =& load_class('Load');
         $this->database =& load_class('Database');
 
-        // Call Main controller (The future instance)
+        // Define main controller (The future instance) as attribute
         $this->controller =& load_class('Controller');
 
-        // Autoload classes
+        // Get helpers and models to autoload
         $autoload = get_config('autoload');
-        $autoload = (is_null($autoload) or (gettype($autoload) !== 'array')) ? [] : $autoload;
         
-        // Helpers
-        if(!isset($autoload['helpers']) or (gettype($autoload['helpers']) !== 'array'))
+        // Create $autoload array if not exist
+        $autoload = is_null($autoload) ? [] : $autoload;
+        
+        // Is an array ?
+        if (gettype($autoload) !== 'array')
+        {
+            throw new system\IZY_Exception('$config[\'autoload\'] doit être un tableau associatif. Exemple&nbsp;: $config[\'autoload\'][\'helpers\']');
+        }
+        
+        // Add 'helpers' key to $autoload array if not exist
+        if (!isset($autoload['helpers']))
         {
             $autoload['helpers'] = [];
         }
+        // If not, check if is an array
+        else if (gettype($autoload['helpers']) !== 'array')
+        {
+            throw new system\IZY_Exception('$autoload[\'helpers\'] doit être un tableau. Exemple&nbsp;: [\'App_helper\']');
+        }
+            
+        // Add 'Izy Url_helper' to autoloaded helpers
         array_push($autoload['helpers'], 'Url_helper');
         
-        foreach($autoload['helpers'] as $helper)
+        // Load helpers
+        foreach ($autoload['helpers'] as $helper)
         {
             load_class($helper, 'helpers');
         }
-        // Models
-        if(isset($autoload['models']) and gettype($autoload['models']) === 'array')
+        
+        // Load models
+        if (isset($autoload['models']))
         {
-            foreach($autoload['models'] as $model)
+            // Check if is an array
+            if (gettype($autoload['models']) !== 'array')
+            {
+                throw new system\IZY_Exception('$autoload[\'models\'] doit être un tableau. Exemple&nbsp;: [\'Blog_model\']');
+            }
+            
+            foreach ($autoload['models'] as $model)
             {
                 load_model($model);
             }
         }
 
-        // Pre controller hook
+        // Call pre_controller class
         $this->hooks->set_hook('pre_controller');
 
-        // Response code
+        // Set Header HTTP response code (depending on the response of the router)
         $this->http->response_code($this->router->response_code);
 
-        // Response controller
+        // Retrieve the controller called by the request
         $response_controller = $this->router->controller;
-
-        if(!empty($this->router->controller))
+        
+        // If controller found by the router
+        if (!empty($this->router->controller))
         {
-
-            // Call controller
+            // Get called controller instance
             $class = new $response_controller();
-
+            
+            // Assign called controller as attribute of instance (main controller)
             get_instance()->{'controller'} = $class;
-
+            
+            // Call controller method passing arguments (depending on the response of the router)
             call_user_func_array(array($class, $this->router->method), $this->router->args);
 
-            // Post controller hook
+            // Call post_controller class
             $this->hooks->set_hook('post_controller');
 
-            // Unset controller
+            // Unset called controller
             unset($class);
 
-            // Headers
+            // Send HTTP Headers
             $this->http->send_headers();
 
-            // Pre display hook
+            // Call pre_display class
             $this->hooks->set_hook('pre_display');
 
-            // Add canonical meta
+            // Add canonical meta tag in $canonicals array
             if($this->router->response_code != '404')
             {
                 $this->output->canonical('canonical', $this->url->request);
             }
 
-            // Output
+            // Write output
             $this->output->_display();
 
-            // Post display hook
+            // Call post_display class
             $this->hooks->set_hook('post_display');
         }
     }
 
     /**
-    * Get single instance of IZY
-    * and return it.
-    * @return Izy
+    * Return the single IZY instance
+    *
+    * @return object IZY instance
     */
     public static function &get_instance()
     {
-        if(is_null(self::$_instance))
+        if (is_null(self::$_instance))
         {
             self::$_instance = new Izy();
         }
--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
+/**
+* This file is part of the Izy_mvc project.
+*
+* Define required constants and autoload framework classes 
+*
+* @package Izy-mvc
+* @copyright 2021 © Pascal Tassel for https://www.izy-mvc.com <contact[@]izy-mvc.com>
+*/
 
 // IZY
 define('IZY', TRUE);
 
-// DIRS
+// Application directories
 define('DIR_PATH', __DIR__ . DIRECTORY_SEPARATOR);
 define('APP_PATH', DIR_PATH . 'app' . DIRECTORY_SEPARATOR);
 define('CONFIG_PATH', APP_PATH . 'config' . DIRECTORY_SEPARATOR);
@@ -11,7 +19,7 @@ define('CONTROLLERS_PATH', APP_PATH . 'controllers' . DIRECTORY_SEPARATOR);
 define('MODELS_PATH', APP_PATH . 'models' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', APP_PATH . 'views' . DIRECTORY_SEPARATOR);
 
-// IZY CLASSES AUTOLOADING
+// Autoload Izy classes
 spl_autoload_register(function ($class_name)
 {
     $namespace = explode('\\', $class_name);
@@ -23,7 +31,13 @@ spl_autoload_register(function ($class_name)
     }
 });
 
-// IZY INSTANCE
-$IZY =& core\Izy::get_instance();
+// Set IZY instance
+try {
+    $IZY =& core\Izy::get_instance();
+}
+catch (core\system\IZY_Exception $e)
+{
+    echo $e;
+}
 
 unset($IZY);
