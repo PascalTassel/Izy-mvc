4e57cdcba55e44717adadaf450026a0dada7ab92
Pascal Tassel <tasselp@gmail.com>
2021-08-01 11:27:00 +0200

    rewrite check_queries()

--- core/helpers/Url_helper.php
+++ core/helpers/Url_helper.php
@@ -4,8 +4,10 @@ namespace core\helpers;
 if(!defined('IZY')) die('DIRECT ACCESS FORBIDDEN');
 
 /**
-* URL helper
-* @author https://www.izy-mvc.com
+* Url utilities
+*
+* @package Izy-mvc
+* @copyright 2021 © Pascal Tassel for https://www.izy-mvc.com <contact[@]izy-mvc.com>
 */
 class IZY_Url_helper
 {
@@ -15,112 +17,137 @@ class IZY_Url_helper
     {
         $this->_IZY =& get_instance();
     }
-
+    
+    /**
+    * Check url queries parameters
+    *
+    * @param array $rules Associative array contains each parameter specifications
+    *
+    * @return void Redirect page if query string is rewrited
+    */
     public function check_queries($rules)
     {
-        // Copie des variables $_GET
-        $q = $this->get_queries();
+        // Get query vars
+        $q = $this->_IZY->url->get_queries();
 
-        // Stockage de l'ordre correct des variables
+        // Store vars order
         $vars_order = array_flip(array_keys($rules));
-
-        foreach ($rules as $name => $value)
-        {
-            // Dépendances, si la variable est dépendante
-            if (isset($q[$name]) && isset($value['depends']))
-            {
-                foreach ($value['depends'] as $field)
-                {
-                    // Si la variable cible n'existe pas, on la crée
-                    if (!isset($q[$field]))
-                    {
-                        $q[$field] = '';
-                    }
-                }
-            }
-
-            // Variables manquantes ?
-            if (!isset($q[$name]))
-            {
-                if (isset($value['required']) && $value['required'] === TRUE)
-                {
-                    $q[$name] = '';
-                }
-                else if (!isset($value['required']) || $value['required'] === FALSE)
-                {
-                    unset($vars_order[$name]);
-                }
-            }
-        }
-
+        
+        // Check and remove wrong vars in $q
         foreach ($q as $name => $value)
         {
-            // Si le paramètre est autorisé
-            if (isset($rules[$name]))
-            {
-                // Valeur string correcte ?
-                if ($rules[$name]['type'] === 'string')
-                {
-                    $value = empty($value) ? '' : $value;
-                    $value = strval($value);
-
-                    if(isset($rules[$name]['accept']))
-                    {
-                        if(gettype($rules[$name]['accept']) == 'array')
-                        {
-                            $q[$name] = !in_array($value, $rules[$name]['accept']) ? $rules[$name]['accept'][0] : $value;
-                        }
-                        else
-                        {
-                            $q[$name] = $value != $rules[$name]['accept'] ? $rules[$name]['accept'] : $value;
-                        }
-                    }
-                }
-                // Valeur de type int
-                else if ($rules[$name]['type'] === 'int')
-                {
-                    $value = empty($value) ? 0 : $value;
-                    $value = intval($value);
-
-                    if (isset($rules[$name]['range']))
-                    {
-                        if ($value <= $rules[$name]['range'][0])
-                        {
-                            $q[$name] = strval($rules[$name]['range'][0]);
-                        }
-                        else if ($value > $rules[$name]['range'][1])
-                        {
-                            $q[$name] =  strval($rules[$name]['range'][1]);
-                        }
-                        else if (($value % $rules[$name]['range'][0]) != 0)
-                        {
-                            $q[$name] =  strval($rules[$name]['range'][0]);
-                        }
-                    }
-                    else if (isset($rules[$name]['min']) && ($q[$name] < $rules[$name]['min']))
-                    {
-                        $q[$name] = strval($rules[$name]['min']);
-                    }
-                    else if (isset($rules[$name]['max']) && ($q[$name] > $rules[$name]['max']))
-                    {
-                        $q[$name] = strval($rules[$name]['max']);
-                    }
-                }
-            }
-            // Sinon, on le supprime
-            else
+            // Wrong var ?
+            if (!isset($rules[$name]))
             {
                 unset($q[$name]);
             }
+            else
+            {
+                // Set default value if empty
+                if ($value === '')
+                {
+                    $value = $rules[$name]['default'];
+                }
+                
+                // Wrong value ?
+                if (preg_match('#^' . $rules[$name]['pattern'] . '$#', $value) === 0)
+                {
+                    $value = $rules[$name]['default'];
+                }
+                // Number rules ?
+                else if (isset($rules[$name]['range']) || isset($rules[$name]['min']) || isset($rules[$name]['max']))
+                {
+                    // Convert value as number
+                    $value = (strpos($value, '.') !== false) ? intval($value) : floatval($value);
+                    
+                    // Range
+                    if (isset($rules[$name]['range']))
+                    {
+                        if ($value < $rules[$name]['range'][0])
+                        {
+                            $value = $rules[$name]['range'][0];
+                        }
+                        else if ($value > $rules[$name]['range'][1])
+                        {
+                            $value = $rules[$name]['range'][1];
+                        }
+                        else if (($value % $rules[$name]['range'][0]) != 0)
+                        {
+                            $value = $rules[$name]['range'][0];
+                        }
+                    }
+                    // Min
+                    else if (isset($rules[$name]['min']) && ($value < $rules[$name]['min']))
+                    {
+                        $value = $rules[$name]['min'];
+                    }
+                    // Max
+                    else if (isset($rules[$name]['max']) && ($value > $rules[$name]['max']))
+                    {
+                        $value = $rules[$name]['max'];
+                    }
+                }
+                
+                $q[$name] = strval($value);
+            }
         }
-
-        // On remet les variables dans l'ordre
+        
+        // Add required vars in $q if not exist
+        foreach ($rules as $var => $params)
+        {
+            $isRequired = isset($params['required']) ? $params['required'] : false;
+            
+            // Add var if required
+            if ($isRequired && !isset($q[$var]))
+            {
+                $q[$var] = $rules[$var]['default'];
+            }
+            
+            // Has dependancies
+            if (isset($q[$var]) && isset($rules[$var]['dependancies']))
+            {
+                foreach ($rules[$var]['dependancies'] as $dependancy)
+                {
+                    // Not isset dependancy ?
+                    if (!isset($q[$dependancy]))
+                    {
+                        $q[$dependancy] = isset($rules[$dependancy]['default']) ? $rules[$dependancy]['default'] : '';
+                        
+                    }
+                }
+            }
+            
+            // Is a dependancy ?
+            if (!isset($q[$var]))
+            {
+                $isDependancy = false;
+                
+                foreach (array_keys($rules) as $rule)
+                {
+                    if (isset($q[$rule]))
+                    {
+                        if (isset($rules[$rule]['dependancies']) && (in_array($var, $rules[$rule]['dependancies'])))
+                        {
+                            $isDependancy = true;
+                            break;
+                        }
+                    }
+                }
+                
+                if ($isDependancy === false)
+                {
+                    unset($vars_order[$var]);
+                }
+            }
+        }
+        
+        // Order $q vars
         $q = array_merge($vars_order, $q);
 
-        // Si la chaîne de sortie est différente
-        if (http_build_query($q) != $_SERVER['QUERY_STRING'])
+        // If query string is rewrited
+        if (http_build_query($q) !== $_SERVER['QUERY_STRING'])
         {
-            // On recharge la page
+            // Reload page
             $url = $this->current_url() . '?' . http_build_query($q);
             $this->_IZY->http->redirect($url, TRUE, 301);
         }
@@ -141,7 +168,7 @@ class IZY_Url_helper
 
     public function query_string()
     {
-        $queries = $this->get_queries;
+        $queries = $this->_IZY->url->get_queries();
         return http_build_query($queries, '', '&amp;');
     }
 
