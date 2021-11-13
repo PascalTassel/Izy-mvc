bf672b844b8102a1c661fd367c6a7895f562d325
Pascal Tassel <tasselp@gmail.com>
2021-11-13 12:36:58 +0100

    updated comments

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,15 @@
 <?php
-
 if(!defined('IZY')) die('DIRECT ACCESS FORBIDDEN');
 
-// Core
-$config['host'] = $_SERVER['HTTP_HOST'];       // www.example.com
+/* CORE configuration variable
+* Define server name, example :
+* $_SERVER['HTTP_HOST'] == 'localhost' ? 'localhost/mon-site' : 'www.mon-site.com'
+*/
+$config['host'] = $_SERVER['HTTP_HOST'];
+
+/* APP configuration variables
+* Add your owns configuration variables into an associative $config[] array, example :
+* $config['foo'] = 'bar';
+* Retrieve a configuration variable everywhere in your code using get_config() function
+* get_config('foo'); // bar
+*/
--- app/config/routes.php
+++ app/config/routes.php
@@ -1,5 +1,4 @@
 <?php
-
 if(!defined('IZY')) die('DIRECT ACCESS FORBIDDEN');
 
 /* URI ROUTING
